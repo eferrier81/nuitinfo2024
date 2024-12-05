6c84e181c9bfef2ba277ca1251f03ca71e5870ff
eferrier81 <enzoferrier81120@gmail.com>
2024-12-05 21:56:55 +0100

    Amélioration de l'interface

--- src/components/MediaPlayer.js
+++ src/components/MediaPlayer.js
@@ -5,19 +5,17 @@ function MediaPlayer() {
     <div className="media-player">
       <h2>Podcast et vidéos</h2>
       <audio controls>
-        <source src="PODCAST-AUDIO-Florian Sévellec.m4a" type="audio/mpeg" />
+        <source
+          src="PODCAST-AUDIO-Florian Sévellec.m4a"
+          type="audio/mpeg"
+        />
         Votre navigateur ne supporte pas l'audio.
       </audio>
-      <video controls width="300">
-        <source src="Florian Sevellec - Oona Layolle-001-SD 480p.mov" type="video/mp4" />
-        Votre navigateur ne supporte pas la vidéo.
-      </video>
-      <video controls width="300">
-        <source src="Frederic Le Moigne - Part 1-SD 480p.mov" type="video/mp4" />
-        Votre navigateur ne supporte pas la vidéo.
-      </video>
-      <video controls width="300">
-        <source src="Frederic Le Moigne - Part 2-SD 480p.mov" type="video/mp4" />
+      <video controls>
+        <source
+          src="Florian Sevellec - Oona Layolle-001-SD 480p.mov"
+          type="video/mp4"
+        />
         Votre navigateur ne supporte pas la vidéo.
       </video>
     </div>
