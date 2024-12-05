83b50e664ad513b822d67f4d09a38d2aad47bc43
eferrier81 <enzoferrier81120@gmail.com>
2024-12-05 20:13:07 +0100

    Ajout des podcasts

--- src/components/MediaPlayer.js
+++ src/components/MediaPlayer.js
@@ -5,11 +5,11 @@ function MediaPlayer() {
     <div className="media-player">
       <h2>Podcast et vidéos</h2>
       <audio controls>
-        <source src="path_to_audio_file.mp3" type="audio/mpeg" />
+        <source src="PODCAST-AUDIO-Florian Sévellec.m4a" type="audio/mpeg" />
         Votre navigateur ne supporte pas l'audio.
       </audio>
       <video controls width="300">
-        <source src="path_to_video_file.mp4" type="video/mp4" />
+        <source src="Florian Sevellec - Oona Layolle-001-SD 480p.mov" type="video/mp4" />
         Votre navigateur ne supporte pas la vidéo.
       </video>
     </div>
